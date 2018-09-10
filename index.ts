ed7f5cd24f479499c68864a1d7ac3677c8f96c97
GibboK <gibbok.coding@gmail.com>
2018-09-10 22:25:56 +0200

    save additional setup

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-const message = 'hello!'
-console.log(message)
+import { IO } from "fp-ts/lib/IO";
+import { existsSync } from "fs";
+import { compose, map } from "ramda";
+
+const fileExist = (path: string) => new IO(() => existsSync(path));
+
+const print = (x: any) =>
+  new IO(() => {
+    // tslint:disable-next-line:no-console
+    console.log(x);
+    return x;
+  });
+
+const cat = compose(
+  // @ts-ignore
+  map(print),
+  fileExist
+);
+
+// tslint:disable-next-line:no-expression-statement
+cat("test");
---
+++ todo.md
@@ -0,0 +1,6 @@
+[] - detect user inputs: source / destination
+[] - list all files for source recursively
+[] - create checksum for source files 
+[] - list all files for destination recursively
+[] - create checksum for destination files 
+[] - do a comparison, source files not present in destination are copied to destination
