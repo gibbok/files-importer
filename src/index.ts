4a788a5e40ce563df1364ba6cef65e666a7e15ef
GibboK <gibbok.coding@gmail.com>
2018-09-11 07:55:31 +0200

    fix setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+// import { IO } from "fp-ts/lib/IO";
+// import { existsSync } from "fs";
+// import { compose, map } from "ramda";
+
+// const fileExist = (path: string) => new IO(() => existsSync(path));
+
+// const print = (x: any) =>
+//   new IO(() => {
+//     // tslint:disable-next-line:no-console
+//     console.log(x);
+//     return x;
+//   });
+
+// const cat = compose(
+//   // @ts-ignore
+//   map(print),
+//   fileExist
+// );
+
+// // tslint:disable-next-line:no-expression-statement
+// cat("test");
+// tslint:disable-next-line:no-console
+console.log("hello!");
