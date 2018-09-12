100afbe03c0fd0448115b12601169b0956cb3188
GibboK <gibbok.coding@gmail.com>
2018-09-12 17:12:34 +0200

    save

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import { Either, left, right } from "fp-ts/lib/Either";
 import { IO } from "fp-ts/lib/IO";
 import { existsSync } from "fs";
+import { curry } from "ramda";
 
 const m = {
-  errorPath: "path is invalid"
+  errPath: "path is invalid"
 };
 
 const log = <T>(obj: T): IO<void> =>
   // tslint:disable-next-line:no-console
   new IO(() => console.log(JSON.stringify(obj)));
 
-const fileExist = (path: string): IO<Either<string, boolean>> =>
-  new IO(() => (!existsSync(path) ? left(m.errorPath) : right(true)));
+const fileExist = curry(
+  (path: string, message: string): IO<Either<string, boolean>> =>
+    new IO(() => (!existsSync(path) ? left(message) : right(true)))
+);
 
-const program = () => fileExist("test").chain(log);
+const program = () => fileExist("test", m.errPath).chain(log);
 
 // tslint:disable-next-line:no-expression-statement
 program().run();
-
-// tslint:disable-next-line:no-console
-console.log("hello!");
