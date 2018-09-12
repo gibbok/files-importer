5fc55d3053424f80ba862f19a2c879ba4f3f68cf
GibboK <gibbok.coding@gmail.com>
2018-09-12 16:55:31 +0200

    save

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import { Either, left, right } from "fp-ts/lib/Either";
 import { IO } from "fp-ts/lib/IO";
 import { existsSync } from "fs";
+
+const m = {
+  errorPath: "path is invalid"
+};
 
 const log = <T>(obj: T): IO<void> =>
   // tslint:disable-next-line:no-console
   new IO(() => console.log(JSON.stringify(obj)));
 
-const fileExist = (path: string): IO<boolean> => new IO(() => existsSync(path));
+const fileExist = (path: string): IO<Either<string, boolean>> =>
+  new IO(() => (!existsSync(path) ? left(m.errorPath) : right(true)));
 
 const program = () => fileExist("test").chain(log);
 
