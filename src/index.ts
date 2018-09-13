2a0ae7721145937d733965fdd8acfda0e69eeea6
GibboK <gibbok.coding@gmail.com>
2018-09-13 08:16:11 +0200

    set tslint

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,14 @@ const m = {
   errPath: "path is invalid"
 };
 
-const log = <T>(obj: T): IO<void> =>
-  // tslint:disable-next-line:no-console
-  new IO(() => console.log(JSON.stringify(obj)));
+const log = <T>(obj: T): IO<void> => new IO(() => console.log(obj));
 
 export const fileExist = curry(
   (path: string, errMessage: string): IO<Either<string, boolean>> =>
     new IO(() => (!pathExistsSync(path) ? left(errMessage) : right(true)))
 );
 
-const program = () => fileExist("invalid-path", m.errPath).chain(log);
+const program = () => fileExist("test", m.errPath).chain(log);
 
 // tslint:disable-next-line:no-expression-statement
 program().run();
