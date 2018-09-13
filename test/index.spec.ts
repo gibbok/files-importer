172ea2b24ce273dd6c02dd48a787b092b8ad006d
GibboK <gibbok.coding@gmail.com>
2018-09-13 15:30:42 +0200

    add os temp directory

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,24 +1,22 @@
 import { create } from "domain";
-import { outputFileSync, removeSync} from "fs-extra";
-import { fileExist } from "../src";
+import { outputFileSync, removeSync } from "fs-extra";
+import { fileExist, walkSync } from "../src";
+import { join} from 'path'
+import { tmpdir} from 'os'
 
-const TEMP_DIR = './tmp/'
+const TEST_DIR = join(tmpdir(), 'files-importer')
 
-const createFile = (path:string) => outputFileSync(path, "hello!");
-const removeFile= (path:string) => removeSync(path)
+const createFile = (path: string) => outputFileSync(path, "hello!");
+const removeFile = (path: string) => removeSync(path)
 
 describe("fileExist", () => {
   const msmErrPath = "path is invalid";
-  const goodPath = `${TEMP_DIR}file.txt`
+  const goodPath = `${TEST_DIR}file.txt`
   const badPath = './invalid-path'
 
-  beforeAll(() => {
-    return createFile(goodPath);
-  });
+  beforeAll(() => createFile(goodPath))
 
-  afterAll(() => {
-    return removeFile(TEMP_DIR);
-  });
+  afterAll(() => removeFile(TEST_DIR))
 
   it("should be valid for good path", () => {
     const fe = fileExist(goodPath, msmErrPath).run();
@@ -29,4 +27,29 @@ describe("fileExist", () => {
     const fe = fileExist(badPath, msmErrPath).run();
     expect(fe.value).toEqual(msmErrPath);
   });
+
+
+
 });
+
+
+describe("walkSynch", () => {
+  const files = [
+    `${TEST_DIR}file1.txt`,
+    `${TEST_DIR}file2.txt`,
+    `${TEST_DIR}/sub/file3.txt`,
+    `${TEST_DIR}/sub/file4.txt`,
+    `${TEST_DIR}/sub/sub/file5.txt`,
+    `${TEST_DIR}/sub/sub/file6.txt`,
+  ]
+
+  beforeAll(() => files.map(createFile))
+
+  afterAll(() => removeFile(TEST_DIR))
+
+  // it('should create list of path', ()=> {
+  //   const ws = walkSync(TEST_DIR)
+  //   expect(ws).toEqual('hey')
+  // })
+
+})
