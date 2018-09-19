8907106ee5d9e8b4abd4e87baac636fe0d71140e
GibboK <gibbok.coding@gmail.com>
2018-09-19 08:37:24 +0200

    save

--- test/log.spec.ts
+++ test/log.spec.ts
@@ -6,12 +6,13 @@ describe("logger", () => {
     const m = "message";
     const log = console.log;
     // tslint:disable-next-line:readonly-array
-    const r: any[] = [];
-    // tslint:disable-next-line:no-object-mutation
-    console.log = (a: any) => r.push(a);
+    const r: string[] = [];
+    // tslint:disable-next-line:no-object-mutation no-expression-statement
+    console.log = (a: string) => r.push(a);
     // tslint:disable-next-line:no-expression-statement
     logger(m).run();
     assert.deepEqual(r, [m]);
+    // tslint:disable-next-line:no-object-mutation no-expression-statement
     console.log = log;
   });
 });
