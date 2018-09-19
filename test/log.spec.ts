de8637933bd608f1e881a13a3dbe625a98a6908e
GibboK <gibbok.coding@gmail.com>
2018-09-19 20:36:00 +0200

    test console.log

--- test/log.spec.ts
+++ test/log.spec.ts
@@ -5,14 +5,10 @@ describe("logger", () => {
   it("should log", () => {
     const m = "message";
     const log = console.log;
-    // tslint:disable-next-line:readonly-array
-    const r: string[] = [];
-    // tslint:disable-next-line:no-object-mutation no-expression-statement
-    console.log = (a: string) => r.push(a);
+    const spy = jest.spyOn(console, "log");
     // tslint:disable-next-line:no-expression-statement
     logger(m).run();
-    assert.deepEqual(r, [m]);
-    // tslint:disable-next-line:no-object-mutation no-expression-statement
-    console.log = log;
+    expect(console.log).toHaveBeenCalled();
+    spy.mockRestore();
   });
 });
