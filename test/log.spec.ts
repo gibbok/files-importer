e7b057844d61a31d26f6db87eaa5b6e1a86f0869
GibboK <gibbok.coding@gmail.com>
2018-09-19 20:37:29 +0200

    save

--- test/log.spec.ts
+++ test/log.spec.ts
@@ -1,13 +1,10 @@
-import * as assert from "assert";
+// tslint:disable: no-expression-statement
 import { logger } from "../src/log";
 
 describe("logger", () => {
   it("should log", () => {
-    const m = "message";
-    const log = console.log;
     const spy = jest.spyOn(console, "log");
-    // tslint:disable-next-line:no-expression-statement
-    logger(m).run();
+    logger("message").run();
     expect(console.log).toHaveBeenCalled();
     spy.mockRestore();
   });
