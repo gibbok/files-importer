3c1b670cf22ff4b0c488e639caf2748893551d3c
GibboK <gibbok.coding@gmail.com>
2018-09-19 20:46:09 +0200

    add test withPrefix

--- src/log.ts
+++ src/log.ts
@@ -3,3 +3,5 @@ import { log } from "fp-ts/lib/Console";
 import { fromIO, TaskEither } from "fp-ts/lib/TaskEither";
 
 export const logger = (x: string): TaskEither<Error, void> => fromIO(log(x));
+
+export const withPrefix = (x: string): string => `\n ${x}`;
--- test/log.spec.ts
+++ test/log.spec.ts
@@ -1,11 +1,19 @@
 // tslint:disable: no-expression-statement
-import { logger } from "../src/log";
+import * as assert from "assert";
+import { logger, withPrefix } from "../src/log";
 
-describe("logger", () => {
+describe("loggger", () => {
   it("should log", () => {
     const spy = jest.spyOn(console, "log");
     logger("message").run();
     expect(console.log).toHaveBeenCalled();
     spy.mockRestore();
+  });
+});
+
+describe("withPrefix", () => {
+  it("should add prefix", () => {
+    const wp = withPrefix("message");
+    assert.equal(wp, "\n message");
   });
 });
