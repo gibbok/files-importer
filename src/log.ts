28aa7648c57c2305f1a8831b3a28f282a509144e
GibboK <gibbok.coding@gmail.com>
2018-10-10 21:45:05 +0200

    add test log

--- src/log.ts
+++ src/log.ts
@@ -2,16 +2,21 @@ import chalk from "chalk";
 import { log } from "fp-ts/lib/Console";
 import { compose } from "fp-ts/lib/function";
 
-export const withPrefix = (s: string): string => `file-importer: ${s}`;
+export const withPrefix = (s: string) => (t: string) =>
+  `file-importer: ${s}: ${t}`;
+
+export const withPrefixSuccess = withPrefix("success");
+
+export const withPrefixError = withPrefix("error");
 
 export const logSuccess = compose(
   log,
   chalk.bold.greenBright,
-  withPrefix
+  withPrefixSuccess
 );
 
 export const logError = compose(
   log,
   chalk.bold.red,
-  withPrefix
+  withPrefixError
 );
--- test/log.spec.ts
+++ test/log.spec.ts
@@ -1,11 +1,30 @@
 // tslint:disable: no-expression-statement
 import * as assert from "assert";
-import { logSuccess, withPrefix } from "../src/log";
+import {
+  logSuccess,
+  withPrefix,
+  withPrefixError,
+  withPrefixSuccess
+} from "../src/log";
 
 describe("withPrefix", () => {
-  it("should add a prefix to a string", () => {
-    const str = withPrefix("m");
-    assert.strictEqual(str, "file-importer: m");
+  it("should add app prefix", () => {
+    const str = withPrefix("x")("y");
+    assert.strictEqual(str, "file-importer: x: y");
+  });
+});
+
+describe("withPrefixSuccess", () => {
+  it("should add success prefix", () => {
+    const str = withPrefixSuccess("m");
+    assert.strictEqual(str, "file-importer: success: m");
+  });
+});
+
+describe("withPrefixSuccess", () => {
+  it("should add error prefix", () => {
+    const str = withPrefixError("m");
+    assert.strictEqual(str, "file-importer: error: m");
   });
 });
 
