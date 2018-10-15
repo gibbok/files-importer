481ecec9a5b7e3e01f13fd4d20a02d45582e65b2
GibboK <gibbok.coding@gmail.com>
2018-10-15 22:56:37 +0200

    create md5

--- src/work.ts
+++ src/work.ts
@@ -1,7 +1,22 @@
+import { createHash } from "crypto";
 import { IOEither, tryCatch } from "fp-ts/lib/IOEither";
+import { createReadStream } from "fs-extra";
 import klawSync from "klaw-sync";
 
 export const walkSync = (
   path: string
 ): IOEither<Error, ReadonlyArray<klawSync.Item>> =>
   tryCatch(() => klawSync(path, { nodir: true }));
+
+// TODO return a fp-ts task
+export const md5 = (path: string) =>
+  new Promise<string>((resolve, reject) => {
+    const hash = createHash("sha1");
+    const rs = createReadStream(path);
+    // tslint:disable-next-line:no-expression-statement
+    rs.on("error", reject);
+    // tslint:disable-next-line:no-expression-statement
+    rs.on("data", chunk => hash.update(chunk));
+    // tslint:disable-next-line:no-expression-statement
+    rs.on("end", () => resolve(hash.digest("hex")));
+  });
