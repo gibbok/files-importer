b59fb1e9fd35e037e611c60444f938dc1570b30a
GibboK <gibbok.coding@gmail.com>
2018-10-23 23:38:48 +0200

    save

--- test/work.spec.ts
+++ test/work.spec.ts
@@ -1,18 +1,45 @@
 import * as assert from "assert";
 import { statSync } from "fs-extra";
 import { BAD_PATH, createFile, removeFile, TEST_DIR } from "../src/testCommon";
-import { md5, walkSync } from "../src/work";
+import { md5, mkPathHash, walkSync } from "../src/work";
+
+const fileNames: ReadonlyArray<string> = [
+  `${TEST_DIR}/file1.txt`,
+  `${TEST_DIR}/file2.txt`,
+  `${TEST_DIR}/sub/file3.txt`,
+  `${TEST_DIR}/sub/file4.txt`,
+  `${TEST_DIR}/sub/sub/file5.txt`,
+  `${TEST_DIR}/sub/sub/file6.txt`
+];
+
+describe("wmkPathHash", () => {
+  beforeAll(() => fileNames.map(createFile));
+
+  afterAll(() => removeFile(TEST_DIR));
+
+  // it("should return left and return a message if it does not walk", () => {
+  //   const ws = walkSync(BAD_PATH);
+  //   assert.strictEqual(ws.isLeft(), true);
+  //   assert.strictEqual(ws.isRight(), false);
+  //   assert.deepStrictEqual(typeof ws.value, "string");
+  // });
+
+  it("should return right xxx", () => {
+    const result = fileNames.map((path: string) => ({
+      path,
+      hash: md5(path).fold(() => "", (hash: string) => hash)
+    }));
+    const ws = walkSync(TEST_DIR);
+    const r = ws.chain(mkPathHash);
+    assert.strictEqual(r.isLeft(), false);
+    assert.strictEqual(r.isRight(), true);
+    assert.deepStrictEqual(r.value, result);
+    // tslint:disable-next-line:no-expression-statement
+    console.log(r);
+  });
+});
 
 describe("walkSynch", () => {
-  const fileNames: ReadonlyArray<string> = [
-    `${TEST_DIR}/file1.txt`,
-    `${TEST_DIR}/file2.txt`,
-    `${TEST_DIR}/sub/file3.txt`,
-    `${TEST_DIR}/sub/file4.txt`,
-    `${TEST_DIR}/sub/sub/file5.txt`,
-    `${TEST_DIR}/sub/sub/file6.txt`
-  ];
-
   beforeAll(() => fileNames.map(createFile));
 
   afterAll(() => removeFile(TEST_DIR));
