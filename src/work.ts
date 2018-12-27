ded0fb9c5ed977d25753aef9730bbbb3b4803974
GibboK <gibbok.coding@gmail.com>
2018-12-27 15:30:14 +0100

    cleanup tslint

--- src/work.ts
+++ src/work.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-let no-expression-statement no-if-statement */
 import { createHash } from "crypto";
 import { head, lefts, zipWith } from "fp-ts/lib/Array";
 import { Either, left, right } from "fp-ts/lib/Either";
@@ -39,28 +40,21 @@ export const mkPathHashList = (
 
 export const md5 = (path: string): Either<string, string> => {
   const BUFFER_SIZE = 8192;
-  // tslint:disable-next-line:no-let
   let fd;
   try {
-    // tslint:disable-next-line:no-expression-statement
     fd = openSync(path, "r");
     const buffer = Buffer.alloc(BUFFER_SIZE);
     const hash = createHash("md5");
-    // tslint:disable-next-line:no-let
     let bytesRead;
     do {
-      // tslint:disable-next-line:no-expression-statement
       bytesRead = readSync(fd, buffer, 0, BUFFER_SIZE, 0);
-      // tslint:disable-next-line:no-expression-statement
       hash.update(buffer.slice(0, bytesRead));
     } while (bytesRead === BUFFER_SIZE);
     return right(hash.digest("hex"));
   } catch (error) {
     return left(error.message);
   } finally {
-    // tslint:disable-next-line:no-if-statement
     if (fd !== undefined) {
-      // tslint:disable-next-line:no-expression-statement
       closeSync(fd);
     }
   }
@@ -74,7 +68,6 @@ export const comparePathHashLists = (
   exclude: intersection(pathHashListSource, pathHashListTarget)
 });
 
-/* tslint:disable:no-expression-statement no-let no-if-statement no-expression-statement */
 export const copyFiles = (include: PathHashList, target: string) => {
   include.forEach(({ path }) => {
     let destination = "";
@@ -88,8 +81,7 @@ export const copyFiles = (include: PathHashList, target: string) => {
       const output = `${target}/${destination}`;
       copySync(path, output);
     } catch (err) {
-      console.error(err);
+      console.log(err);
     }
   });
 };
-/* tslint:enable:no-expression-statement no-let no-if-statement */
