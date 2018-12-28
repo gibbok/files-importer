e1eb543fe6c173430a39dbfc38ab7c22cce9b5f7
GibboK <gibbok.coding@gmail.com>
2018-12-28 17:09:47 +0100

    make code more fp

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-expression-statement */
 import { checkArgs, checkPath, checkPathsUnequal } from "./check";
 import { logError } from "./log";
 import { Errors } from "./types";
@@ -5,22 +6,12 @@ import { Errors } from "./types";
 const printErrors = (errors: Errors) => errors.forEach(x => logError(x).run());
 
 const program = (args: ReadonlyArray<string>) =>
-  checkArgs(args).fold(printErrors, x =>
-    checkPathsUnequal(x).fold(printErrors, y => {
-      const target = checkPath(y.target);
-      const source = checkPath(y.source);
-      // tslint:disable-next-line:no-if-statement
-      if (target.isRight() && source.isRight()) {
-        // tslint:disable-next-line:no-expression-statement
-        console.log("success");
-      } else {
-        const errorsTarget = target.isLeft() ? target.value : [];
-        const errorsSource = source.isLeft() ? source.value : [];
-        // tslint:disable-next-line:no-expression-statement
-        printErrors(errorsTarget.concat(errorsSource));
-      }
+  checkArgs(args).fold(printErrors, ts =>
+    checkPathsUnequal(ts).fold(printErrors, ({ target, source }) => {
+      checkPath(target).fold(printErrors, _targetOk =>
+        checkPath(source).fold(printErrors, _sourceOk => _sourceOk)
+      );
     })
   );
 
-// tslint:disable-next-line:no-expression-statement
 program(process.argv);
