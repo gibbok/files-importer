99c773fd3e70dd1d171042831a12cc1a104313a2
GibboK <gibbok.coding@gmail.com>
2018-12-28 17:14:10 +0100

    save

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@
 import { checkArgs, checkPath, checkPathsUnequal } from "./check";
 import { logError } from "./log";
 import { Errors } from "./types";
+import { walkSync } from "./work";
 
 const printErrors = (errors: Errors) => errors.forEach(x => logError(x).run());
 
 const program = (args: ReadonlyArray<string>) =>
   checkArgs(args).fold(printErrors, ts =>
     checkPathsUnequal(ts).fold(printErrors, ({ target, source }) => {
-      checkPath(target).fold(printErrors, _targetOk =>
-        checkPath(source).fold(printErrors, _sourceOk => _sourceOk)
+      checkPath(target).fold(printErrors, targetOk =>
+        checkPath(source).fold(printErrors, sourceOk => {
+          walkSync(targetOk).fold(printErrors, _targetWalked =>
+            walkSync(sourceOk).fold(printErrors, _sourceWalked => _sourceWalked)
+          );
+        })
       );
     })
   );
