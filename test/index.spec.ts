19375d8076e6880b32614b27d86264ef2e9860ee
GibboK <gibbok.coding@gmail.com>
2018-12-30 20:36:59 +0100

    add test main

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-expression-statement */
 import * as assert from "assert";
-import { pathExistsSync, readdirSync } from "fs-extra";
+import { mkdirSync, pathExistsSync, readdirSync } from "fs-extra";
 import { main } from "../src";
 import { createFile, removeFile, TEST_DIR } from "../src/test-common";
 
@@ -16,20 +16,20 @@ describe("program", () => {
 
   afterAll(() => removeFile(TEST_DIR));
 
-  it("should run the program adding file into the os, source has one more file than target", () => {
+  it("should run the program adding one file into the os, source has one more file than target", () => {
     main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`]);
     assert.deepEqual(pathExistsSync(`${TEST_DIR}/target/source/sub/file2.txt`), true);
   });
 });
 
 describe("program", () => {
-  const fileNames2: ReadonlyArray<string> = [
+  const fileNames: ReadonlyArray<string> = [
     `${TEST_DIR}/source/file1.txt`,
     `${TEST_DIR}/source/sub/file2.txt`,
     `${TEST_DIR}/target/file1.txt`,
     `${TEST_DIR}/target/sub/file2.txt`
   ];
-  beforeAll(() => fileNames2.map(createFile));
+  beforeAll(() => fileNames.map(createFile));
 
   afterAll(() => removeFile(TEST_DIR));
 
@@ -37,5 +37,26 @@ describe("program", () => {
     main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target`), ["file1.txt", "sub"]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target/sub`), ["file2.txt"]);
+  });
+});
+
+describe("program", () => {
+  const fileNames: ReadonlyArray<string> = [
+    `${TEST_DIR}/source/file1.txt`,
+    `${TEST_DIR}/source/sub/file2.txt`,
+    `${TEST_DIR}/source/sub/sub/file3.txt`
+  ];
+  beforeAll(() => {
+    fileNames.map(createFile);
+    mkdirSync(`${TEST_DIR}/target`);
+  });
+
+  afterAll(() => removeFile(TEST_DIR));
+
+  it("should run the program adding three files into the os, source has files and target has none", () => {
+    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`]);
+    assert.deepEqual(readdirSync(`${TEST_DIR}/target/source`), ["file1.txt", "sub"]);
+    assert.deepEqual(readdirSync(`${TEST_DIR}/target/source/sub`), ["file2.txt", "sub"]);
+    assert.deepEqual(readdirSync(`${TEST_DIR}/target/source/sub/sub`), ["file3.txt"]);
   });
 });
