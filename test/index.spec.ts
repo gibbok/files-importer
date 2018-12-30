2c14bbe2a0d6fca90244b22f5b030f7df8500191
GibboK <gibbok.coding@gmail.com>
2018-12-30 20:51:47 +0100

    cleanup test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,6 +4,8 @@ import { mkdirSync, pathExistsSync, readdirSync } from "fs-extra";
 import { main } from "../src";
 import { createFile, removeFile, TEST_DIR } from "../src/test-common";
 
+afterEach(() => removeFile(TEST_DIR));
+
 describe("program", () => {
   const fileNames: ReadonlyArray<string> = [
     `${TEST_DIR}/source/file1.txt`,
@@ -12,9 +14,8 @@ describe("program", () => {
     `${TEST_DIR}/target/file1.txt`,
     `${TEST_DIR}/target/file3.txt`
   ];
-  beforeAll(() => fileNames.map(createFile));
 
-  afterAll(() => removeFile(TEST_DIR));
+  beforeAll(() => fileNames.map(createFile));
 
   it("should run the program adding one file into the os, source has one more file than target", () => {
     main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`]);
@@ -29,11 +30,10 @@ describe("program", () => {
     `${TEST_DIR}/target/file1.txt`,
     `${TEST_DIR}/target/sub/file2.txt`
   ];
+
   beforeAll(() => fileNames.map(createFile));
 
-  afterAll(() => removeFile(TEST_DIR));
-
-  it("should run the program without adding any files into the os, source and target have the same files", () => {
+  it("should run the program without adding any files into the os, source and target have same files", () => {
     main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target`), ["file1.txt", "sub"]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target/sub`), ["file2.txt"]);
@@ -46,12 +46,11 @@ describe("program", () => {
     `${TEST_DIR}/source/sub/file2.txt`,
     `${TEST_DIR}/source/sub/sub/file3.txt`
   ];
+
   beforeAll(() => {
     fileNames.map(createFile);
     mkdirSync(`${TEST_DIR}/target`);
   });
-
-  afterAll(() => removeFile(TEST_DIR));
 
   it("should run the program adding three files into the os, source has files and target has none", () => {
     main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`]);
