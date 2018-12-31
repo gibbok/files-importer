91b89be630c872012836cbec71b45746f8fbfb75
GibboK <gibbok.coding@gmail.com>
2018-12-31 20:28:37 +0100

    rename tests

--- test/log.spec.ts
+++ test/log.spec.ts
@@ -16,28 +16,28 @@ import {
 } from "../src/log";
 
 describe("withPrefix", () => {
-  it("should add app prefix", () => {
+  it("should add a prefix to a string", () => {
     const str = withPrefix("x")("y");
     assert.strictEqual(str, "file-importer: x: y");
   });
 });
 
 describe("withPrefixSuccess", () => {
-  it("should add success prefix", () => {
+  it("should add a `success` prefix to a string", () => {
     const str = withPrefixSuccess("m");
     assert.strictEqual(str, "file-importer: success: m");
   });
 });
 
 describe("withPrefixError", () => {
-  it("should add error prefix", () => {
+  it("should add an `error` prefix to a string", () => {
     const str = withPrefixError("m");
     assert.strictEqual(str, "file-importer: error: m");
   });
 });
 
 describe("withPrefixInfo", () => {
-  it("should add info prefix", () => {
+  it("should add an `info` prefix to a string", () => {
     const str = withPrefixInfo("m");
     assert.strictEqual(str, "file-importer: info: m");
   });
@@ -62,37 +62,37 @@ const testConsoleLogs = (
 };
 
 describe("logSuccess", () => {
-  it("should log in console success with prefix", () => {
+  it("should log in console with `success` prefix", () => {
     testConsoleLog(logSuccess, "file-importer: success: message");
   });
 });
 
 describe("logError", () => {
-  it("should log in console error with error prefix", () => {
+  it("should log in console with `error` prefix", () => {
     testConsoleLog(logError, "file-importer: error: message");
   });
 });
 
 describe("logInfo", () => {
-  it("should log in console info with info prefix", () => {
+  it("should log in console with `info` prefix", () => {
     testConsoleLog(logInfo, "file-importer: info: message");
   });
 });
 
 describe("logErrors", () => {
-  it("should log in console errors with prefixes", () => {
+  it("should log in console messages with `error` prefixes", () => {
     testConsoleLogs(logErrors, ["error 1", " error 2"]);
   });
 });
 
 describe("logSuccesses", () => {
-  it("should log in console successes with prefixes", () => {
+  it("should log in console messages with `success` prefixes", () => {
     testConsoleLogs(logSuccesses, ["success 1", " success 2"]);
   });
 });
 
 describe("logInfos", () => {
-  it("should log in console infos with prefixes", () => {
+  it("should log in console messages with `infos` prefixes", () => {
     testConsoleLogs(logInfos, ["info 1", " info 2"]);
   });
 });
