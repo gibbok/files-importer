332e58eca94391256279ee4e35721dfe1cb7a8aa
GibboK <gibbok.coding@gmail.com>
2019-01-05 22:07:58 +0100

    add prompts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 /* tslint:disable:no-expression-statement */
+import { log } from "fp-ts/lib/Console";
+import { fromNullable } from "fp-ts/lib/Option";
+import { prompt } from "prompts";
 import { checkArgs, checkPathsInequality, checkPathSource, checkPathTarget } from "./check";
 import { logErrors, logInfos, logSuccesses } from "./log";
 import { comparePathHashLists, copyFiles, mkPathHashList, walkSync } from "./work";
@@ -15,13 +18,31 @@ export const main = (args: ReadonlyArray<string>) =>
           walkSync(sourceResolved).fold(logErrors, sourceWalked => {
             walkSync(targetResolved).fold(logErrors, targetWalked => {
               mkPathHashList(sourceWalked).fold(logErrors, sourcePathHashList => {
-                mkPathHashList(targetWalked).fold(logErrors, targetPathHashList => {
+                mkPathHashList(targetWalked).fold(logErrors, async targetPathHashList => {
                   const { include, exclude } = comparePathHashLists(
                     sourcePathHashList,
                     targetPathHashList
                   );
-                  copyFiles(include, targetResolved).fold(logErrors, logSuccesses);
-                  logInfos(exclude.map(x => x.path));
+                  log(
+                    `file-importer: info: ${
+                      include.length
+                    } new files has been found. Do you want to copy them to \`target\` directory?`
+                  ).run();
+                  logInfos(include.map(x => x.path));
+
+                  const response =
+                    process.env.NODE_ENV === "test"
+                      ? { value: true }
+                      : await prompt({
+                          type: "confirm",
+                          name: "value",
+                          message: "file-importer: info: Can you confirm?",
+                          initial: true
+                        });
+                  fromNullable(response.value).mapNullable(_x => {
+                    copyFiles(include, targetResolved).fold(logErrors, logSuccesses);
+                    logInfos(exclude.map(x => x.path));
+                  });
                 });
               });
             });
