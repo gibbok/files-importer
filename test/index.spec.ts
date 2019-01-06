4f55b1dd1453554fc5bb56f2a7f7290b4204f9fe
GibboK <gibbok.coding@gmail.com>
2019-01-06 13:01:02 +0100

    revert to prompts

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-expression-statement */
 import * as assert from "assert";
 import { mkdirSync, pathExistsSync, readdirSync } from "fs-extra";
-import { main } from "../src";
+import { main, PROMPT_CONFIG } from "../src";
 import { createFile, removeFile, TEST_DIR } from "../src/test-common";
 // jest.mock("readline");
 
@@ -17,7 +17,7 @@ describe("program", () => {
   afterEach(() => removeFile(TEST_DIR));
 
   it("should perform files comparison between `source` and `target` folders, if `source` has one more file than `target` add that one file to `target`", () => {
-    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`], "test");
+    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`], PROMPT_CONFIG, "test");
     assert.deepEqual(pathExistsSync(`${TEST_DIR}/target/source/sub/file2.txt`), true);
   });
 });
@@ -33,7 +33,7 @@ describe("program", () => {
   afterEach(() => removeFile(TEST_DIR));
 
   it("should perform files comparison between `source` and `target` folders, if `source` and `target` have the same files, no files should be added to `target`", () => {
-    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`], "test");
+    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`], PROMPT_CONFIG, "test");
     assert.deepEqual(readdirSync(`${TEST_DIR}/target`), ["file1.txt", "sub"]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target/sub`), ["file2.txt"]);
   });
@@ -52,7 +52,7 @@ describe("program", () => {
   afterEach(() => removeFile(TEST_DIR));
 
   it("should perform files comparison between `source` and `target` folders, if `source` has tree new files and `target` has none, the tree files should be added to `target`", () => {
-    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`], "test");
+    main(["npm", "start", `${TEST_DIR}/source`, `${TEST_DIR}/target`], PROMPT_CONFIG, "test");
     assert.deepEqual(readdirSync(`${TEST_DIR}/target/source`), ["file1.txt", "sub"]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target/source/sub`), ["file2.txt", "sub"]);
     assert.deepEqual(readdirSync(`${TEST_DIR}/target/source/sub/sub`), ["file3.txt"]);
