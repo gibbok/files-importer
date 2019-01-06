d60267f90925866b8da1fb8804a560c9f4ef708b
GibboK <gibbok.coding@gmail.com>
2019-01-06 11:46:58 +0100

    save

--- __mocks__/readline.ts
+++ __mocks__/readline.ts
@@ -1,15 +1,7 @@
-// tslint:disable
-// module.exports = {
-//   createInterface: jest.fn().mockReturnValue({
-//       question: jest.fn()
-//           .mockImplementationOnce((_questionText, cb) => cb('y'))
-//       })
-//   })
-// };
-
-
-module.exports ={
-  createInterface :jest.fn().mockReturnValue({
-    question:jest.fn().mockImplementationOnce((_questionTest, cb)=> cb('y'))
+// tslint:disable:no-object-mutation no-expression-statement
+module.exports = {
+  createInterface: jest.fn().mockReturnValue({
+    question: jest.fn().mockImplementationOnce((_questionTest, cb) => cb("y")),
+    close: jest.fn().mockImplementationOnce(() => undefined)
   })
-}
+};
