6980a171fdc0c53fe4bb500a31f2dd2b8f216336
GibboK <gibbok.coding@gmail.com>
2019-01-12 17:20:33 +0100

    save

--- __mocks__/readline.ts
+++
@@ -1,7 +0,0 @@
-// tslint:disable:no-object-mutation no-expression-statement
-module.exports = {
-  createInterface: jest.fn().mockReturnValue({
-    question: jest.fn().mockImplementationOnce((_questionTest, cb) => cb("y")),
-    close: jest.fn().mockImplementationOnce(() => undefined)
-  })
-};
