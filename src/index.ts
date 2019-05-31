931e1f10f85b2594df4482343dc887f240f95d77
GibboK <gibbok@users.noreply.github.com>
2019-05-31 23:18:09 +0200

    Code refactory use chain instead of fold (#7)
    Rewrite nested .fold with .chain.

--- src/index.ts
+++ src/index.ts
@@ -16,28 +16,28 @@ export const PROMPT_CONFIG: PromptObject = {
  * Terminal usage example: `npm start ~/Documents/source ~/Documents/target`
  */
 export const main = (args: ReadonlyArray<string>, promptConfig: PromptObject, env?: string) =>
-  checkArgs(args).fold(logErrors, ts => {
-    checkPathsInequality(ts).fold(logErrors, ({ source, target }) => {
-      checkPathSource(source).fold(logErrors, sourceResolved => {
-        checkPathTarget(target).fold(logErrors, targetResolved => {
-          walkSync(sourceResolved).fold(logErrors, sourceWalked => {
-            walkSync(targetResolved).fold(logErrors, targetWalked => {
-              mkPathHashList(sourceWalked).fold(logErrors, sourcePathHashList => {
-                mkPathHashList(targetWalked).fold(logErrors, async targetPathHashList => {
-                  await promptConfirmationCopy(
-                    sourcePathHashList,
-                    targetPathHashList,
-                    targetResolved,
-                    promptConfig,
-                    env
-                  );
-                });
+  checkArgs(args)
+    .chain(pathsSourceTarget => checkPathsInequality(pathsSourceTarget))
+    .map(({ source, target }) => {
+      checkPathSource(source)
+        .chain(sourceResolved => walkSync(sourceResolved))
+        .chain(sourceWalked => mkPathHashList(sourceWalked))
+        .fold(logErrors, sourcePathHashList =>
+          checkPathTarget(target)
+            .chain(targetResolved => walkSync(targetResolved))
+            .chain(targetWalked => mkPathHashList(targetWalked))
+            .fold(logErrors, async targetPathHashList => {
+              checkPathTarget(target).map(async targetResolved => {
+                await promptConfirmationCopy(
+                  sourcePathHashList,
+                  targetPathHashList,
+                  targetResolved,
+                  promptConfig,
+                  env
+                );
               });
-            });
-          });
-        });
-      });
+            })
+        );
     });
-  });
 
 main(process.argv, PROMPT_CONFIG, process.env.NODE_ENV);
